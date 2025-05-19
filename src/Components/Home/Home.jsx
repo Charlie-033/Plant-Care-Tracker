97c62cb48d608d2616032db1ce29c2cd6aa17afc
Charlie-033 <ahmedeleus1@gmail.com>
2025-05-19 19:07:04 +0600

    Initialize Plant Care Tracker Client Side, install react router, tailwindcss and daisyui

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className='text-6xl font-semibold text-center top-6'>Plant Care Tracker Client</h2>
+        </div>
+    );
+};
+
+export default Home;
