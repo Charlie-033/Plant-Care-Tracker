814fa194d805bfd8b8dc742ac02b4c15f78167b3
Charlie-033 <ahmedeleus1@gmail.com>
2025-05-24 19:13:20 +0600

    Project Complete with all requirements

--- src/Provider/PrivateRoute.jsx
+++ src/Provider/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import AuthContext from './AuthContext';
 import Loader from '../Components/Others/Loader';
 import { Navigate, useLocation } from 'react-router';
@@ -6,14 +6,18 @@ import { toast } from 'react-toastify';
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
-    const location = useLocation()
+    const location = useLocation();
+    useEffect(() => {
+        if(!loading && (!user || !user.email)){
+            toast.info('You are not logged in! Please login first.');
+        }
+    },[loading, user])
 
     if(loading) return <Loader/>
 
     if(user && user.email){
         return children;
     } else {
-        toast.info('You are not logged in! Please login first.');
         return <Navigate to="/auth/login" state={location.pathname} replace />
     }
 };
